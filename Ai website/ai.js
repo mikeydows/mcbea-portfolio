265696db27a2b452cbdac878c723cc4abe1d5bfc
mikeydows <michaelokang8@gmail.com>
2025-05-14 22:21:34 +0000

    Add files via upload

---
+++ Ai website/ai.js
@@ -0,0 +1,10 @@
+
+  let userName = prompt("User Name");
+  const password = prompt("Password");
+  let age = prompt("your Age");
+
+  if(age <= 17){
+    alert("underage");
+  } else{
+    alert("youre older");
+  };
